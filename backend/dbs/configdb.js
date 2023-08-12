7d98269acec660c2078c4222a349c157f1b598fc
Shoneeth <shoneeth.kothakonda@gmail.com>
2023-08-12 10:50:50 +0530

    Update configdb.js

--- backend/dbs/configdb.js
+++ backend/dbs/configdb.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://shoneeth:kvssr2002@cluster0.qcholsm.mongodb.net/E-comm?retryWrites=true&w=majority");
+mongoose.connect(PASSWORD);
